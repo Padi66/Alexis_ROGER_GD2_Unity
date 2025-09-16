9c104d7a348825dc9dc40288f834291cbb8343fb
Padi66 <ar.padawan@gmail.com>
2025-09-16 14:16:22 +0100

    [ARO] Initialize Project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody _rb;
+    private float _horizontalMovement;
+    private float _verticalMovement;
+    private Vector3 _movement;
+    [SerializeField] private float _speed = 2.0f;
+    
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        
+    }
+    
+    void Update()
+    {
+        _horizontalMovement = Input.GetAxis("Horizontal");
+        _verticalMovement = Input.GetAxis("Vertical");
+        _movement=new Vector3(_horizontalMovement, 0.0f, _verticalMovement);
+        _movement.Normalize();
+        _movement *= _speed;
+        _movement.y = _rb.linearVelocity.y;
+        if (_rb != null)
+        {
+            _rb.linearVelocity = _movement;
+        }
+        else
+        {
+            Debug.LogError("No RigidBody Attached !");
+        }
+
+    }
+}
