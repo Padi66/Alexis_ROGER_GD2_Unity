1422967e4994c4fd0c386575d2bf4d439cc64a52
Padi66 <ar.padawan@gmail.com>
2025-10-09 14:19:50 +0100

    [ARO] cours091025 matin

---
+++ Assets/Scripts/ParticuleController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class ParticuleController : MonoBehaviour
+{
+    void start()
+    {
+        Destroy(gameObject,t:5f);
+    }
+}
--- Assets/Scripts/TargetSoft.cs
+++ Assets/Scripts/TargetSoft.cs
@@ -1,15 +1,59 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class TargetSoft : MonoBehaviour
 {
    [SerializeField] private int _targetValue = 1;
+   [SerializeField] private float _shadowDuration = 3f;
+   [SerializeField] private GameObject _particleEffect;
+   private float _shadowTimer = 0f;
+   private bool _isInShadows = false;
    private void OnTriggerEnter(Collider other)
    {
       if (other.gameObject.GetComponent<Player_Collect>() != null)
       {
          other.gameObject.GetComponent<Player_Collect>().UpdateScore(_targetValue);
-         Destroy(gameObject);
+         //Destroy(gameObject);
+         //TODO : hide Target
+         ToggleVisibility(false);
+         //TODO : Start Timer
+         //_isInShadows = true;
+         //Instantiate(_particleEffect.transform.position Quaternion.identity);
+         StartCoroutine(routine: ShadowTimerControl());
       }
    }
+
+   private void ToggleVisibility(bool newVisibility)
+   {
+      GetComponent<MeshRenderer>().enabled = newVisibility;
+      GetComponent<Collider>().enabled = newVisibility;
+   }
+   //TODO: Timer by deltatime
+   /*private void Update()
+   {
+      if (_isInShadows)
+      {
+         _shadowTimer += Time.deltaTime;
+         if (_shadowTimer >= _shadowDuration)
+         {
+            //TODO: Show Target
+            ToggleVisibility(true);
+            //TODO: Stop Timer
+            _shadowTimer = 0f;
+            _isInShadows = false;
+        }
+      
+      }
+            
+    }*/
+   
+   //TODO : Timer by coroutine
+   private IEnumerator ShadowTimerControl()
+   {
+      //yield return new WaitForEndOfFrame()
+      yield return new WaitForSeconds(_shadowDuration);
+      ToggleVisibility(true);
+   }
+       
 }
